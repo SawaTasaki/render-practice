1d2d63eb1805fdd8b859a72ee451df828a4ddc83
sawa <xj558y2d0290acc@gmail.com>
2025-04-07 16:19:34 +0900

    DB接続設定を変更

--- backend/src/dbClient.js
+++ backend/src/dbClient.js
@@ -1,14 +1,31 @@
-import pkg from "pg";
-const { Client } = pkg;
+const { Client } = require("pg");
 
+// 環境変数からデータベースURLを取得
 const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DB_HOST,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-client.connect();
+client
+  .connect()
+  .then(() => console.log("データベースに接続していますよ"))
+  .catch((err) => console.error("データベース接続エラーですよ:", err.stack));
 
 export default client;
+
+// import pkg from "pg";
+// const { Client } = pkg;
+
+// const client = new Client({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// client.connect();
+
+// export default client;
