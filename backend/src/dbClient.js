7ee468112ebf076a17e446a93e2fd627f51cf9e7
sawa <xj558y2d0290acc@gmail.com>
2025-04-07 16:47:59 +0900

    DB削除

--- backend/src/dbClient.js
+++ backend/src/dbClient.js
@@ -1,12 +1,7 @@
 import pkg from "pg";
 const { Client } = pkg;
 
-// 環境変数からデータベースURLを取得
 const client = new Client({
-  connectionString: process.env.DB_HOST,
-  ssl: {
-    rejectUnauthorized: false,
-  },
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
@@ -14,24 +9,6 @@ const client = new Client({
   database: process.env.DB_NAME,
 });
 
-client
-  .connect()
-  .then(() => console.log("データベースに接続していますよ"))
-  .catch((err) => console.error("データベース接続エラーですよ:", err.stack));
+client.connect();
 
 export default client;
-
-// import pkg from "pg";
-// const { Client } = pkg;
-
-// const client = new Client({
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-// });
-
-// client.connect();
-
-// export default client;
