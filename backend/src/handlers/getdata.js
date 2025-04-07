585e9046d3645d52b832370a9cca9ce879e937d7
sawa <xj558y2d0290acc@gmail.com>
2025-04-07 16:11:22 +0900

    エラー文変更

--- backend/src/handlers/getdata.js
+++ backend/src/handlers/getdata.js
@@ -4,11 +4,13 @@ const getData = async (client, req, res) => {
     if (result.rows.length > 0) {
       return res.json(result.rows);
     } else {
+      console.log("データが見つかりませんでした...。404")
       return res
         .status(404)
         .json({ message: "データが見つかりませんでした。" });
     }
   } catch (err) {
+    console.log("データが見つかりませんでした...。500")
     return res.status(500).json({ error: err.stack });
   }
 };
