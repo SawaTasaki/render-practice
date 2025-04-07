e475afe508b5f9464a88734e96b5fa377f5b7a6a
sawa <xj558y2d0290acc@gmail.com>
2025-04-07 14:40:14 +0900

    second commit

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`サーバーはポート ${PORT} で動作しています`);
+});
